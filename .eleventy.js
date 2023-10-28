4f6132b5b7c8982e0c4c4a3e06f0bbef155b334f
Oleg Fedak <olegfedak@gmail.com>
2023-10-28 21:33:43 +0300

    Update .eleventy.js

--- .eleventy.js
+++ .eleventy.js
@@ -11,98 +11,99 @@ itemsCollection
 
 */
 
-module.exports = function(eleventyConfig, options={
+module.exports = function (eleventyConfig, options = {
   categoryVar: "categories",
   itemsCollection: "posts",
 }) {
-    const categoryCollection = options.categoryCollection || options.categoryVar;
-    const categoryVar = options.categoryVar;
-    const itemsCollection = options.itemsCollection;
-    const perPageCount = options.perPageCount || 5;
+  const categoryCollection = options.categoryCollection || options.categoryVar;
+  const categoryVar = options.categoryVar;
+  const itemsCollection = options.itemsCollection;
+  const perPageCount = options.perPageCount || 5;
 
-    // Creates the collection
-    eleventyConfig.addCollection(categoryCollection, function(collections) {
+  // Creates the collection
+  eleventyConfig.addCollection(categoryCollection, function (collections) {
 
-      const posts = collections.getFilteredByTag(options.itemsCollection)
-      let tagArray = getCategoryKeys(posts, options);
+    const posts = collections.getFilteredByTag(options.itemsCollection)
+    let tagArray = getCategoryKeys(posts, options);
 
-      const categoriesWithPosts = tagArray.map(category => {
-        let filteredPosts = posts.filter(post => {
-          if (!post.data[categoryVar]) return false
-          return post.data[categoryVar].includes(category)}
-          ).flat();
-          
-        return { 
-          'title': category,
-          'slug': slugify(category),
-          [itemsCollection || 'posts']: [ ...filteredPosts ],
-        };
-      })
-      console.log(`\x1b[32m[Dynamic Categories] Created Collection ${categoryCollection} with ${categoriesWithPosts.length} items`, '\x1b[0m')
-      return categoriesWithPosts;
+    const categoriesWithPosts = tagArray.map(category => {
+      let filteredPosts = posts.filter(post => {
+        if (!post.data[categoryVar]) return false
+        return post.data[categoryVar].includes(category)
+      }
+      ).flat();
+
+      return {
+        'title': category,
+        'slug': slugify(category),
+        [itemsCollection || 'posts']: [...filteredPosts],
+      };
     })
+    console.log(`\x1b[32m[Dynamic Categories] Created Collection ${categoryCollection} with ${categoriesWithPosts.length} items`, '\x1b[0m')
+    return categoriesWithPosts;
+  })
 
-    eleventyConfig.addCollection(`${categoryCollection}ByPage`, function(collection) {
-        // Get unique list of all tags currently in use
-        const posts = collection.getFilteredByTag(options.itemsCollection)
+  eleventyConfig.addCollection(`${categoryCollection}ByPage`, function (collection) {
+    // Get unique list of all tags currently in use
+    const posts = collection.getFilteredByTag(options.itemsCollection)
 
-        // Get each item that matches the tag and add it to the tag's array, chunked by paginationSize
-        let paginationSize = perPageCount;
-        let tagMap = [];
-        let tagArray = getCategoryKeys(posts, options);
+    // Get each item that matches the tag and add it to the tag's array, chunked by paginationSize
+    let paginationSize = perPageCount;
+    let tagMap = [];
+    let tagArray = getCategoryKeys(posts, options);
 
-        for(let tagName of tagArray) {
-          const filteredPosts = posts.filter(post => {
-            if (!post.data[categoryVar]) return false
-            return post.data[categoryVar].includes(tagName)}
-            ).flat();
+    for (let tagName of tagArray) {
+      const filteredPosts = posts.filter(post => {
+        if (!post.data[categoryVar]) return false
+        return post.data[categoryVar].includes(tagName)
+      }
+      ).flat();
 
-          let tagItems = filteredPosts.reverse();
-          let pagedItems = _.chunk(tagItems, paginationSize);
-          const totalPages = Math.ceil(filteredPosts.length / perPageCount)
-          for( let pageNumber = 0, max = pagedItems.length; pageNumber < max; pageNumber++) {
-            const currentNumber = pageNumber + 1
-            tagMap.push({
-              slug: slugify(tagName),
-              title: tagName,
-              totalPages,
-              [itemsCollection || 'posts']: pagedItems[pageNumber],
-              permalinkScheme: `${slugify(tagName)}${currentNumber > 1 ? `/${currentNumber}` : ''}/index.html`,
-              pages: {
-                current: currentNumber,
-                next: currentNumber != totalPages && currentNumber + 1,
-                previous: currentNumber > 1 &&  currentNumber - 1
-              }
-            });
+      let tagItems = filteredPosts.reverse();
+      let pagedItems = _.chunk(tagItems, paginationSize);
+      const totalPages = Math.ceil(filteredPosts.length / perPageCount)
+      for (let pageNumber = 0, max = pagedItems.length; pageNumber < max; pageNumber++) {
+        const currentNumber = pageNumber + 1
+        tagMap.push({
+          slug: slugify(tagName),
+          title: tagName,
+          totalPages,
+          [itemsCollection || 'posts']: pagedItems[pageNumber],
+          permalinkScheme: `${slugify(tagName)}${currentNumber > 1 ? `/${currentNumber}` : ''}/index.html`,
+          pages: {
+            current: currentNumber,
+            next: currentNumber != totalPages && currentNumber + 1,
+            previous: currentNumber > 1 && currentNumber - 1
           }
-        }
-        // Return a two-dimensional array of items, chunked by paginationSize
-        return tagMap;
-    });
-      
-    eleventyConfig.addShortcode('pagination', function(page) {
-      const {pages} = page
-      const {current, next, previous} = pages
-      const allNumbers = Array.from(Array(page.totalPages).keys())
+        });
+      }
+    }
+    // Return a two-dimensional array of items, chunked by paginationSize
+    return tagMap;
+  });
 
-      const pageList = allNumbers.map(number => {
-        const pageNumber = number+1;
-        const urlBase = current == 1 ? './' : '../'
-        const url = pageNumber === 1 ? `${urlBase}` : `${urlBase}${pageNumber}`
-        const isCurrent = pageNumber === current;
+  eleventyConfig.addShortcode('pagination', function (page) {
+    const { pages } = page
+    const { current, next, previous } = pages
+    const allNumbers = Array.from(Array(page.totalPages).keys())
 
-        return `
+    const pageList = allNumbers.map(number => {
+      const pageNumber = number + 1;
+      const urlBase = current == 1 ? './' : '../'
+      const url = pageNumber === 1 ? `${urlBase}` : `${urlBase}${pageNumber}`
+      const isCurrent = pageNumber === current;
+
+      return `
         <span class="pagination-page${isCurrent ? ' currentPage' : ''}">
           ${isCurrent ? pageNumber : `<a href="${url}">${pageNumber}</a>`}
         </span>
         `}).join('')
 
-      const nextHref = next ? `<a class="pagination-page" href="${(current != 1) ? '../' : ''}${next}">Next Page</a>`: '<span class="pagination-page">Next Page</span>'
-      const previousHref = previous ? `<a class="pagination-page" href="../${previous == 1 ? '' : previous}">Previous Page</a>`: '<span class="pagination-page">Previous Page</span>'
-      const markup = `<nav class="pagination">${previousHref}${pageList}${nextHref} </nav>`
-      return markup
-    })  
-
-      
+    const nextHref = next ? `<a class="pagination-page" href="${(current != 1) ? '../' : ''}${next}">Next Page</a>` : '<span class="pagination-page">Next Page</span>'
+    const previousHref = previous ? `<a class="pagination-page" href="../${previous == 1 ? '' : previous}">Previous Page</a>` : '<span class="pagination-page">Previous Page</span>'
+    const markup = `<nav class="pagination">${previousHref}${pageList}${nextHref} </nav>`
+    return markup
+  })
+}
 
 }
